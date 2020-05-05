29bc28bca202aad71e96b8bfae384b69b157bb71
Matheus Abreu <matheusromulo@live.com>
2020-05-05 19:12:21 -0300

    Criando Classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xadrez_console.Tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+
+            p = new Posicao(1, 2);
+
         }
     }
 }
